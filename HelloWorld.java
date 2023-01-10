1c1d14c5647a04912cdffdd52ef6b5c4e4d119f3
Daniel Zheng <65824308+danielz-777@users.noreply.github.com>
2023-01-10 13:55:29 -0800

    hello world file changed

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,10 @@ public class HelloWorld
     public static void main(String[] args) 
     {
         System.out.println("Hello world");
+        System.out.println("Hi");
+
+        System.out.println("This is a hello world file");
     }
+
 }
-
-
 
